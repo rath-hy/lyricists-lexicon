daf3b365f2743ce6a24cbfd4b4af1cec5c4ead33
rath hy <puthyrathy@berkeley.edu>
2026-03-07 08:32:04 -0800

    add ssl configuration

--- server/util/db.js
+++ server/util/db.js
@@ -1,17 +1,36 @@
+// const { Sequelize } = require('sequelize')
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'postgres',
+// })
+
+// const connectToDatabase = async () => {
+//   try {
+//     await sequelize.authenticate()
+//     console.log('Connected to the database')
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error)
+//     process.exit(1)
+//   }
+// }
+
+// module.exports = { sequelize, connectToDatabase }
+
 const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 })
 
 const connectToDatabase = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connected to the database')
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-    process.exit(1)
-  }
+  await sequelize.authenticate()
+  console.log('Connected to database')
 }
 
 module.exports = { sequelize, connectToDatabase }
